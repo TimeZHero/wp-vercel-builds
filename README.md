40084d5ec4af58b65f14fdbbb630dbc4fef0fdfa
Stefano Fasoli <stefanofasoli17@gmail.com>
2023-04-13 18:47:29 +0200

    feat: working lambda code

--- README.md
+++ README.md
@@ -31,15 +31,19 @@
 Vercel currently has a limit of 20 configurable webhooks, which may not be enough for all your projects and environments. You can work around it by setting up a Lambda function on AWS (or equivalent) as the general webhook receiver for all your projects and proxy the request to the correct server.
 
 A `lambda.mjs` file is included in the repo as an example of a possible implementation, but don't forget to:
+
 1. Configure the environment variable `VERCEL_SIGNATURE` with the value provided by Vercel
-2. Review the configuration array with all your projects and environments
+2. Review the "project" object with all your projects and environments
 
 ## Contents
+
 1. Can view builds, with their status, date and duration
 2. Customer-side debugging is possible by reviewing previous versions from the url
 3. The commit SHA is included to help spot any regression bug
+4. Lambda function file to work around the Vercel limitation of 20 webhooks
 
 ## Roadmap
+
 1. Add a badge with simple polling to let the user know when a build completes
 2. Add a button to allow the customer to pin a previous deployment as production
 3. Include customer-friendly build logging in case of failure. May help when the issue is located in the Frontend application
--- lambda.mjs
+++ lambda.mjs
@@ -1,33 +1,82 @@
 import crypto from 'crypto';
+import https from 'https';
 
-async function verifySignature(req) {
-    const signature = crypto
-        .createHmac('sha1', process.env.VERCEL_SIGNATURE)
-        .update(req.body)
-        .digest('hex');
-        
-    return signature === req.headers['x-vercel-signature'];
+
+function performRequest(options, body) {
+  return new Promise((resolve, reject) => {
+    const req = https.request(options, (res) => {
+      console.log(`statusCode: ${res.statusCode}`);
+
+      let result = ''
+
+      res.on('data', (chunk) => {
+        result += chunk;
+      });
+
+      res.on('end', () => resolve(result))
+    });
+
+    req.on('error', (error) => {
+      console.error(error);
+      reject(error)
+    });
+
+    req.write(body);
+    req.end();
+  })
 }
 
-export const handler = async(event) => {
-    
-    if (! await verifySignature(event)) {
-        return {
-            statusCode: 403,
-            body: 'Unauthorized.'
-        }
-    }
+async function verifySignature(req) {
+  const signature = crypto
+    .createHmac('sha1', process.env.VERCEL_SIGNATURE)
+    .update(req.body)
+    .digest('hex');
 
-    const body = JSON.parse(event.body)
-    const project = body.payload.project.id
-    const environment = body.payload.deployment.meta.gitlabCommitRef
+  return signature === req.headers['x-vercel-signature'];
+}
 
-    // TODO: switch case to each project or something..
-    // TODO: post request
+export const handler = async (event) => {
+
+  if (! await verifySignature(event)) {
+    console.log('Error: Request not from Vercel')
 
     return {
-        statusCode: 200,
-        body: {},
-    };
-};
+      statusCode: 403,
+      body: {
+        message: 'Unauthorized.'
+      }
+    }
+  }
 
+  const body = JSON.parse(event.body)
+  const project = body.payload.project.id
+  const environment = body.payload.deployment.meta.gitlabCommitRef
+  const projects = {
+    'myProjectKey': {
+      'main': 'mysite.com',
+      'preprod': 'preprod.mysite.com'
+    }
+  }
+  const domain = projects[project][environment]
+
+  if (!domain) {
+    console.log('No domain was found. Maybe you need to update the lambda?')
+
+    return {
+      statusCode: 404,
+      body: {}
+    }
+  }
+
+  const options = {
+    hostname: domain,
+    port: 443,
+    path: '/wp-json/builds/update',
+    method: 'POST',
+    headers: {
+      'x-vercel-signature': event.headers['x-vercel-signature']
+    }
+  };
+
+  console.log(await performRequest(options, event.body))
+};
